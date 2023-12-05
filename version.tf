b028a1ce081408cb3330b2bee22b8022c8ebf88f
kiwi-07 <109615915+kiwi-07@users.noreply.github.com>
2023-12-05 15:12:38 +0530

    Update version.tf

--- version.tf
+++ version.tf
@@ -1,8 +1,7 @@
 terraform {
+  required_version = ">= 0.12.9"
+
   required_providers {
-    delphix = {
-      source  = "delphix-integrations/delphix"
-      version = "3.1.0"
-    }
+    delphix        = ">= 3.1.0"
   }
 }
